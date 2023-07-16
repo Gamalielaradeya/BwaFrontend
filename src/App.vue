2a3ffe25f49dc1be83e76d22990e582b8966e33b
Gamalielaradeya <Gamalielshroud@gmail.com>
2023-07-16 22:05:36 +0700

    update katalog dan list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,61 @@
-<script setup>
+<script>
 import navbar from './components/navbar.vue';
 import katalog from './components/katalog.vue';
+import list from './components/list.vue';
+
+export default {
+  components: {
+    navbar,
+    katalog,
+    list,
+  },
+  data() {
+    return {
+      products: [
+        { id: 0, name: 'Vanilla JavaScript Pada Website Development', harga: 'Rp 0', category: 'Coding' },
+        { id: 1, name: 'Flutter Mobile Apps', harga: 'Rp 0', category: 'Coding' },
+        { id: 2, name: 'CSS Website Design', harga: 'Rp 0', category: 'Coding' },
+        { id: 3, name: 'PHP Laravel Web Development', harga: 'Rp 0', category: 'Coding' },
+        { id: 4, name: 'React JavaScript', harga: 'Rp 0', category: 'Coding' },
+        { id: 5, name: 'Vue JavaScript Framework', harga: 'Rp 0', category: 'Coding' },
+        { id: 6, name: 'PHP Dasar Website Development', harga: 'Rp 0', category: 'Coding' },
+        { id: 7, name: 'CSS Tailwind Web Design', harga: 'Rp 0', category: 'Coding' },
+        { id: 8, name: 'ES6 Dasar', harga: 'Rp 0', category: 'Coding' },
+        { id: 9, name: 'Learn Figma For Beginner', harga: 'Rp 0', category: 'Coding' },
+        { id: 10, name: 'UI Design: Wireframe to Visual Design', harga: 'Rp 0', category: 'Design' },
+        { id: 11, name: 'Blender: Design 3D Illustration', harga: 'Rp 0', category: 'Design' },
+        { id: 12, name: 'Adobe Illustration: How to create Icon for UI', harga: 'Rp 0', category: 'Design' },
+      ],
+      filteredProducts: [],
+    };
+  },
+  methods: {
+    handleCategoryChange(selectedCategories) {
+      if (selectedCategories.includes('All')) {
+        this.filteredProducts = this.products;
+      } else {
+        this.filteredProducts = this.products.filter((product) =>
+          selectedCategories.includes(product.category)
+        );
+      }
+    },
+  },
+};
 </script>
 
+
 <template>
+  <div class="bg-slate-100">
   <navbar />
-  <div class="pt-10">
-    <katalog /> 
+  <div class="pt-10 w-10/12 border-4 mx-auto">
+    <div class="flex flex-col text-2xl font-bold text-slate-800">
+      <span>Browse Kelas</span>
+      <span>Sesuai Karir Kamu</span>
+    </div>
+    <div class="flex flex-row pt-10">
+      <katalog @category-change="handleCategoryChange" />
+      <list :filteredProducts="filteredProducts" />
+    </div>
   </div>
-  
+</div>
 </template>
---
+++ src/components/list.vue
@@ -0,0 +1,78 @@
+<script>
+export default {
+  props: {
+    filteredProducts: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+
+<template>
+    <div class="flex w-10/12">
+      <div class="w-full grid grid-cols-3 px-2 gap-x-4 gap-y-6">
+        <transition-group name="fade" mode="out-in">
+        <div v-for="product in filteredProducts" :key="product.id">
+          <div class="flex flex-col h-full">
+            <a href="" class="flex items-center justify-center relative">
+              <img src="https://buildwithangga.com/themes/front/images/play_btn_blue.svg" alt="tombolpley"
+                class="absolute">
+              <img src="https://buildwithangga.com/storage/assets/thumbnails/freelancer_basic_thumb.png" alt="gambar" class="rounded-t-xl">
+            </a>
+            <div class="px-2 bg-white py-4 rounded-xl">
+              <div class="h-14">
+                <span class="font-semibold text-xl" id="name">{{ product.name }}</span>
+              </div>
+              <div class="mt-2">
+                <span class="text-slate-700" id="harga">{{ product.harga }}</span>
+              </div>
+              <div class="flex flex-row items-center justify-between space-x-1 w-full">
+                <div class="flex flex-row">
+                  <div class="flex flex-row">
+                    <img src="https://buildwithangga.com/themes/front/images/ic_star.svg" alt=""
+                      class="w-6">
+                    <img src="https://buildwithangga.com/themes/front/images/ic_star.svg" alt=""
+                      class="w-6">
+                    <img src="https://buildwithangga.com/themes/front/images/ic_star.svg" alt=""
+                      class="w-6">
+                    <img src="https://buildwithangga.com/themes/front/images/ic_star.svg" alt=""
+                      class="w-6">
+                    <img src="https://buildwithangga.com/themes/front/images/ic_star.svg" alt=""
+                      class="w-6">
+                  </div>
+                  <div>
+                    <span>(13,840)</span>
+                  </div>
+                </div>
+                <div>
+                  <img src="https://buildwithangga.com/themes/front/images/ic_lv_ez.svg" alt="" class="w-6">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+    </transition-group>
+      </div>
+    </div>
+  </template>
+  
+  <style scoped>
+  .fade-item {
+    transition: opacity 0.5s;
+  }
+  
+  .fade-enter-active {
+    transition: opacity 0.5s;
+  }
+  .fade-leave-active {
+    transition: opacity 0.5s;
+  }
+  
+  .fade-enter,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  </style>
+ 
